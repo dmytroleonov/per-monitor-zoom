667294babbd1b231a6ced98dc1dcaad415981770
Dmytro Leonov <leonovdmitriy02@gmail.com>
2025-09-13 20:52:08 +0300

    feat: persist monitors and with their default zoom levels

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,13 +1,84 @@
-import { createSignal } from "solid-js";
+import { createEffect, createSignal, For, onMount } from "solid-js";
+import browser from "webextension-polyfill";
+
+type Monitor = {
+  width: number;
+  height: number;
+  defaultZoomLevel: number;
+};
+
+const zoomLevels = [1, 1.5];
+const defaultZoomLevel = 1;
 
 export function Popup() {
-  const [count, setCount] = createSignal(0);
+  const [monitors, setMonitors] = createSignal<Monitor[]>([]);
+
+  onMount(async () => {
+    const data = (await browser.storage.local.get("monitors")) as {
+      monitors?: Monitor[];
+    };
+    setMonitors(data.monitors ?? []);
+    createEffect(() => {
+      browser.storage.local.set({ monitors: monitors() });
+    });
+  });
+
+  const addCurrentMonitor = async (): Promise<void> => {
+    const { width, height } = window.screen;
+
+    const exists = monitors().some(
+      (monitor) => monitor.width === width && monitor.height === height,
+    );
+    if (exists) {
+      return;
+    }
+    setMonitors([...monitors(), { width, height, defaultZoomLevel }]);
+  };
+
+  const changeDefaultZoomLevel = (
+    monitor: Monitor,
+    newDefaultZoomLevel: number,
+  ): void => {
+    setMonitors(
+      monitors().map((m) =>
+        m.width === monitor.width && m.height === monitor.height
+          ? { ...m, defaultZoomLevel: newDefaultZoomLevel }
+          : m,
+      ),
+    );
+  };
 
   return (
-    <div style={{ width: "200px", height: "100px" }}>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count()}
-      </button>
+    <div
+      style={{
+        width: "200px",
+        display: "flex",
+        "flex-direction": "column",
+        gap: "8px",
+      }}
+    >
+      <button onClick={addCurrentMonitor}>Add current monitor</button>
+      <div>
+        <For each={monitors()} fallback={<p>No monitors detected</p>}>
+          {(monitor) => (
+            <div>
+              {monitor.width}x{monitor.height}
+              <select
+                value={monitor.defaultZoomLevel}
+                onChange={(e) =>
+                  changeDefaultZoomLevel(monitor, +e.target.value)
+                }
+              >
+                <For each={zoomLevels}>
+                  {(zoomLevel) => (
+                    <option value={zoomLevel}>{zoomLevel}</option>
+                  )}
+                </For>
+              </select>
+            </div>
+          )}
+        </For>
+      </div>
     </div>
   );
 }
